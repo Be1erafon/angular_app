1a70f4df1b4f93972e26804709cde18b1263766c
Be1erafon <belerafon89@gmail.com>
2020-04-13 23:13:16 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { PostComponent } from './post/post.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule
